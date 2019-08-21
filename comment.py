ac5f72b954b80235f59a71d9d711cffa8dc8d171
Maha <proma.1487@gmail.com>
2019-08-21 14:19:58 +0300

    comments

---
+++ comment.py
@@ -0,0 +1,18 @@
+#comment lesson
+print("Comments can be used to explain Python code.")
+print("can write comments at same line")#Comment at same line
+
+# print("Comments does not have to be text to explain the code, it can also be used to prevent Python from executing code")
+print("Comments can be used to prevent Python from executing code")
+
+#Multi Line Comments
+#Python does not really have a syntax for multi-line comments.
+#To add a multiline comment, you could insert a # for each line.
+print("Multi Line Comments")
+
+"""
+Multiline String
+Since Python will ignore string literals that are not assigned to a variable,
+you can add a multiline string (triple quotes) in your code and place your comment inside it.
+"""
+print("Will Done")

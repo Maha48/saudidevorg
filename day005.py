9a45a9df7650b87ea6aa5655974e42c1bf8f7efb
Maha <proma.1487@gmail.com>
2019-08-24 09:09:42 +0300

    week1 challenge

---
+++ day005.py
@@ -0,0 +1,40 @@
+import string
+x= "apple " 
+y = "orange " 
+z= "limon "
+basket = x + y + z
+c=" ".join(basket)
+print(basket)
+print(basket.split())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(len(basket))
+# h=basket.split()
+# print(h)
+# # print (" ".join(h))
+# import string
+# c = list(map(int,h))
+# print(c)
+
+
+
+
+
+
+
+
+

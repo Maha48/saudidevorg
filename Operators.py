263f743c5d93a486a1efcdc9dfcbbeaf306c5f49
Maha <proma.1487@gmail.com>
2019-08-29 19:52:44 +0300

    day10 operators

---
+++ Operators.py
@@ -0,0 +1,43 @@
+#Operators are used to perform operations on variables and values.
+# Python divides the operators in the following groups:
+# Arithmetic operators.
+#Assignment operators.
+#Comparison operators.
+#Logical operators.
+#Identity operators.
+#Membership operators.
+#Bitwise operators.
+
+
+#Python Arithmetic Operators: Arithmetic operators are used with numeric values to perform common mathematical operations.
+x=4
+y=2
+g=x*y
+# print( x * y)#8
+j="x*y ={} "
+print(j.format(g))
+
+a=7
+b=4
+g=a//b
+print("a//b = {}".format(g))#1
+
+
+
+#Python Assignment Operators
+#Assignment operators are used to assign values to variables.
+
+x=6
+x+=4
+print(x)#10
+x=2
+x *=11
+print(x) #22
+
+
+
+# Python Comparison Operators :Comparison operators are used to compare two values.
+c=4
+d=1
+print(c!=d)#true
+print(c<d) #false
